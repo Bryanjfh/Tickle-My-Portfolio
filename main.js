83b12e8e595a4449fce2f7dbb975ddead51c225f
akkiel <akkiel@iastate.edu>
2018-03-24 16:55:32 -0500

    merge

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-function fakeLogin(){
+// AWS GATEWAY:
+var aws = "https://2ji8y26hd8.execute-api.us-east-1.amazonaws.com/api";
+
+function fakeLogin() {
     document.location.href = "home.html";
     return true;
 }
 
 function addClick() {
-    alert("send put to server.");
-    return true;
+    var symbol = document.getElementById("stockName").value;
+    var shares = document.getElementById("stockShares").value;
+    var price = document.getElementById("stockPrice").value;
+    // Make  'POST' call to API
+    var xhttp = new XMLHttpRequest();
+    var json = {};
+    json.symbol = symbol;
+    json.shares = shares;
+    json.price = price;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.responseText);
+        }
+    };
+    xhttp.open("POST", aws + "/addvalue/crypto/user/adam", true);
+    xhttp.setRequestHeader("Content-type", "application/json; charset=UTF-8", "Access-Control-Allow-Origin");
+    xhttp.send(JSON.stringify(json));
+    return false;
 }
 
 function displayStocks() {
     alert("Display Stocks in List.");
+    // Make 'GET' call to API
     return true;
 }
 
@@ -22,4 +42,3 @@ function displayAll() {
     alert("Display all holdings in List.");
     return true;
 }
-
