54ab57468db4b6e8dcec5baab7ab1d754f71a496
Christian <christiandcf@gmail.com>
2018-03-25 00:18:09 -0500

    working on crypto

--- tickle/app.py
+++ tickle/app.py
@@ -78,7 +78,7 @@ def portfolio_value(user):
     return total_stock_value(stocks)
 
 
-@app.route('/addvalue/crypto/user/{user}', methods=['POST', 'OPTIONS'])
+@app.route('/addvalue/portfolio/user/{user}', methods=['POST', 'OPTIONS'])
 def add_value(user):
     stuff = app.current_request.json_body
     return {user: stuff}
@@ -89,6 +89,78 @@ def total_stock_value(stock_symbols):
     symbols = ""
     for symbol in stock_symbols:
         symbols += (symbol['symbol'] + ",")
+    results = json.loads(requests.get("https://api.iextrading.com/1.0/stock/market/batch?symbols={}&types=price".format(symbols)).content)
+    for result in results:
+        quant = int()
+        for symbol in stock_symbols:
+            if symbol['symbol'].encode('ascii') == result:
+                quant = int(symbol['quantity'])
+                symbol['value'] = results[result]['price']
+            price = results[result]['price']
+
+
+        value += (price * quant)
+    return {'value': value, 'stocks': stock_symbols}
+
+#
+# CRYPTO
+#
+
+# @app.route('/testput')
+# def test_put():
+#     dynamodb.Table('Portfolios').put_item(
+#         Item={
+#             'user': 'Chino',
+#             'stocks': [{'symbol': 'BRK.B', 'price': 205, 'quantity': 5},
+#                        {'symbol': 'SNAP', 'price': 1, 'quantity': 150}]
+#         })
+#     return {'hello': 'world'}
+
+
+@app.route('/addcryptos/user/{user}', methods=['POST', 'OPTIONS'])
+def test_update_crypto(user):
+    cryptos2 = list()
+    json_input = app.current_request.json_body['cryptos']
+    for crypto in json_input:
+        new_crypto = json.dumps(crypto)
+        new_crypto = json.loads(new_crypto)
+        new_crypto2 = {
+            'symbol': new_crypto['symbol'], 'price': new_crypto['price'], 'quantity': new_crypto['quantity']}
+        cryptos2.append(new_crypto2)
+
+    dynamodb.Table('Portfolios').update_item(
+        Key={
+            'user': user
+        },
+        UpdateExpression="SET cryptos = list_append(if_not_exists(cryptos, :empty_list), :cryptovals)",
+        ExpressionAttributeValues={":cryptovals": cryptos2, ":empty_list": []}
+    )
+    return 200
+
+@app.route('/value/crypto/user/{user}', methods=['GET'])
+def portfolio_value(user):
+    stuff = dynamodb.Table('Portfolios').get_item(
+        Key={'user': user}
+    )
+    cryptos = list()
+    for crypto in stuff['Item']['cryptos']:
+        stocks.append({'symbol': crypto['symbol'], 'quantity': crypto['quantity'], 'paid': crypto['price']})
+    return total_stock_value(cryptos)
+
+
+@app.route('/addvalue/crypto/user/{user}', methods=['POST', 'OPTIONS'])
+def add_value_crypto(user):
+    stuff = app.current_request.json_body
+    return {user: stuff}
+
+
+def total_crypto_value(crypto_symbols):
+    value = 0
+    symbols = ""
+    for symbol in crypto_symbols:
+        symbols += (symbol['symbol'] + ",")
+
+ # PARSING CRYPTO
     results = json.loads(requests.get("https://api.iextrading.com/1.0/stock/market/batch?symbols={}&types=price".format(symbols)).content)
     for result in results:
         quant = int()
