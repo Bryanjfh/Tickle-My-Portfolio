39a5ce6f715f9d5feb78de45120d86e8dfc3d864
bryanjfh <bryanjfh@iastate.edu>
2018-03-24 23:13:02 -0500

    Wrong...

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // AWS GATEWAY:
-var aws = "https://2ji8y26hd8.execute-api.us-east-1.amazonaws.com/api";
+// var aws = "https://2ji8y26hd8.execute-api.us-east-1.amazonaws.com/api";
+var rip = "https://2ji8y26hd8.execute-api.us-east-1.amazonaws.com/api/addstocks/user/adam";
 
 function fakeLogin() {
     document.location.href = "home.html";
@@ -12,25 +13,57 @@ function addClick() {
     var price = document.getElementById("stockPrice").value;
     // Make  'POST' call to API
     var xhttp = new XMLHttpRequest();
-    var json = [{"symbol": "BRK.B", "price": 205, "quantity": 5},
-    {"symbol": "SNAP", "price": 1, "quantity": 150}
+    var json = [{"stocks": [{"symbol": "BRK.B", "price": 205, "quantity": 5},
+                      {"symbol": "SNAP", "price": 1, "quantity": 150}]}
    ];
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             alert(this.responseText);
         }
     };
-    xhttp.open("POST", aws + "/addvalue/crypto/user/adam", true);
+    // xhttp.open("POST", aws + "/addvalue/crypto/user/adam", true);
+    xhttp.open("POST", rip, true);
     xhttp.setRequestHeader("Content-type", "application/json; charset=UTF-8",);
     xhttp.send(JSON.stringify(json));
     return false;
 }
 
 function displayStocks() {
-    alert("Display Stocks in List.");
+    // alert("Display Stocks in List.");
     // Make 'GET' call to API
 
-    return true;
+    var totalRows = 5;
+    var cellsInRow = 4;
+    var min = 1;
+    var max = 10;
+
+        // function drawTable() {
+            // get the reference for the body
+            var div1 = document.getElementById('div1');
+
+            // creates a <table> element
+            var tbl = document.createElement("table");
+
+            // creating rows
+            for (var r = 0; r < totalRows; r++) {
+                var row = document.createElement("tr");
+
+    	     // create cells in row
+                 for (var c = 0; c < cellsInRow; c++) {
+                    var cell = document.createElement("td");
+                    var cellText = document.createTextNode("hi");
+                    cell.appendChild(cellText);
+                    row.appendChild(cell);
+                }
+
+    	           tbl.appendChild(row); // add the row to the end of the table body
+            }
+
+         div1.appendChild(tbl); // appends <table> into <div1>
+    // }
+    // window.onload=drawTable;
+
+    // return true;
 }
 
 function displayCryptos() {
